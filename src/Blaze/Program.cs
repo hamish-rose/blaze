d673301039899a8180bb752e58a50588d64e76ab
Hamish Rose <hamishr@protonmail.com>
2020-12-18 14:45:02 +1300

    add balance

--- src/Blaze/Program.cs
+++ src/Blaze/Program.cs
@@ -3,6 +3,7 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Nethereum.Web3;
 
 namespace Blaze
 {
@@ -13,7 +14,7 @@ namespace Blaze
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddSingleton(sp => new Web3("https://mainnet.infura.io/v3/61af35ec8ca94a5bb1bc6d8fee06dc37"));
 
             await builder.Build().RunAsync();
         }
